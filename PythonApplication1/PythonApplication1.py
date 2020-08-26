bfd72721be8fba75f86f1469e39cc8bad76fbda6
p8aggie <44847193+p8aggie99@users.noreply.github.com>
2020-08-26 09:44:10 -0500

    Add project files.

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,24 @@
+#print('Hello, world!')
+
+#print("This line will be printed.")
+
+#myint = 7
+#print(myint)
+
+#myfloat = 7.2
+#print(myfloat)
+#myfloat1 = float(7)
+#print(myfloat1)
+
+#one = 1
+#two = 2
+#three = one + two
+#print(three)
+
+#a, b = 3, 4
+#print(a, b)
+
+one = 1
+two = 2
+hello = "hello"
+print(one + two + hello )
